5deaa58a3efe9567bbc32b9e383063f5f6e67601
YOGEESH K P <yogeesh793@gmail.com>
2024-10-20 01:40:03 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,29 @@
+# Arithmetic Operators
+a=5
+b=20
+print('addition',a+b)
+print('Subraction',a-b)
+print('Multiplication',a*b)
+print('Exponent',a**b)
+print('Division',a/b)
+print('Modulus',a%b)
+print('Floor Division',a//b)
+
+# Unary Operators
+c=10
+print(+c)
+print(-c)
+
+# Relational / Comparison Operator
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+# Assignment Operator
+m=5
+
+m+=20
+print(m)
